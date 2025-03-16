04399393a5da1a94c9ee8a2707d4b41e6fd3107d
Rompelhd <75935831+rompelhd@users.noreply.github.com>
2025-03-16 02:55:14 +0100

    Create main.c

---
+++ kernel/main.c
@@ -0,0 +1,24 @@
+#include "print.h"
+
+void kernel_main() {
+    print_clear();
+
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("Welcome to ");
+
+    print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
+    print_str("FuckOFF kernel ");
+
+    print_set_color(PRINT_COLOR_BLUE, PRINT_COLOR_BLACK);
+    print_str("v.0.1 ");
+
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("by ");
+
+    print_set_color(PRINT_COLOR_RED, PRINT_COLOR_BLACK);
+    print_str("Rompelhd!\n");
+
+    while (1) {
+        __asm__ volatile ("hlt");
+    }
+}
